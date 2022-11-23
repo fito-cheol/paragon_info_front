3d6e31c63ba79403cce895bbdc460bf0278b2565
김철 <dmsghs92@gmail.com>
2022-11-23 11:16:45 +0900

    Add ItemCard

--- src/components/card/ItemCard.tsx
+++ src/components/card/ItemCard.tsx
@@ -1,59 +1,82 @@
-import { Card, CardHeader, CardContent, Avatar, Typography } from '@mui/material';
 import React, { useState } from 'react';
+import { Card, CardHeader, CardContent, Avatar, Typography } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
 
-type Item = {
-  이름: string;
-  name: string;
-  카테고리A: string;
-  일회성: string;
-  가격: number;
-  '사용 효과 (w. tag)': string;
-  '사용 효과': string;
-  '사용 효과 쿨타임': number | string;
-  '지속 효과 (w. tag)': string;
-  '지속 효과': string;
-  '지속 효과 쿨타임': number | string;
-  '물리 공격력': number | string;
-  '물리 관통력': number | string;
-  '물리 방어력 무효화': number | string;
-  마법공격력: number | string;
-  '마법 관통력': number | string;
-  '마법 방어력 무효화': number | string;
-  '치명타 확률': number | string;
-  '공격 속도': number | string;
-  '생명력 흡수': number | string;
-  '물리 피해 흡혈': number | string;
-  '마법 피해 흡혈': number | string;
-  '모든 피해 흡혈': number | string;
-  '물리 방어력': number | string;
-  '마법 방어력': number | string;
-  체력: number | string;
-  마나: number | string;
-  '체력 재생': number | string;
-  '마나 재생': number | string;
-  '군중 제어 저항': number | string;
-  '재사용 대기 시간 감소': number | string;
-  '이동 속도': number | string;
-  '골드 가치': number;
-  하위템_List: string;
-  '이미지 path': string;
-};
+import { Item, AttributeArray } from '../../utils/commonTypes';
+import attributeList from '../../assets/item/attribute_language.json';
 
 export interface IItemCardProps {
   item: Item;
 }
 
 export default function ItemCard({ item }: IItemCardProps) {
-  const imagePath = `../../../assets/item/ITEM_IMAGE/${item.name}.png`;
-  console.log(imagePath);
+  const [currentImage, setCurrentImage] = React.useState<string>();
+  import(`../../assets/item/ITEM_IMAGE/${item.name}.png`).then(image => setCurrentImage(image.default));
+  const [attribueArray, setAttribueArray] = React.useState<AttributeArray>(attributeList);
+  const attributesHTML = attribueArray.map(attribute => {
+    if (item[attribute.attribute_kr]) {
+      return (
+        <Grid container spacing={0} xs={12}>
+          <Grid xs='auto'>
+            <img
+              src={`ITEM_OPTION_IMAGE/${attribute.attribute_en}.png`}
+              alt={attribute.attribute_en}
+              loading='lazy'
+              width={20}
+              height={20}
+            />
+          </Grid>
+          <Grid xs='auto'>
+            <Typography variant='body2' color='text.secondary' key={attribute.attribute_en}>
+              {item[attribute.attribute_kr]} {attribute.attribute_kr}
+            </Typography>
+          </Grid>
+        </Grid>
+      );
+    } else {
+      return <React.Fragment key={attribute.attribute_en}></React.Fragment>;
+    }
+  });
   return (
-    <Card>
-      <CardHeader avatar={<Avatar alt={item.name} src={imagePath} sx={{ width: 24, height: 24 }} />} />
+    <Card className='itemCard'>
       <CardContent>
-        <Typography variant='body2' color='text.secondary'>
-          This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of
-          frozen peas along with the mussels, if you like.
-        </Typography>
+        <Grid container spacing={1}>
+          <Grid xs='auto'>
+            <img src={currentImage} alt={item['name']} loading='lazy' width={70} height={70} />
+          </Grid>
+          <Grid container spacing={0}>
+            <Grid xs={12}>
+              <Typography variant='body1' color='text.secondary'>
+                {item['이름']}
+              </Typography>
+            </Grid>
+            <Grid xs='auto'>
+              <img src='gold_icon.png' alt={item['name']} loading='lazy' width={30} height={23} />
+            </Grid>
+            <Grid xs={6}>
+              <Typography variant='body1' color='text.secondary'>
+                {item['가격']}
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid container spacing={0}>
+          {attributesHTML}
+        </Grid>
+        {item['지속 효과'] ? (
+          <Typography variant='body2' color='text.secondary'>
+            지속 효과: {item['지속 효과']} {item['지속 효과 쿨타임'] ? `(${item['지속 효과 쿨타임']}초)` : ''}
+          </Typography>
+        ) : (
+          <React.Fragment />
+        )}
+        {item['사용 효과'] ? (
+          <Typography variant='body2' color='text.secondary'>
+            사용 효과: {item['사용 효과']} {item['사용 효과 쿨타임'] ? `(${item['사용 효과 쿨타임']}초)` : ''}
+          </Typography>
+        ) : (
+          <React.Fragment />
+        )}
       </CardContent>
     </Card>
   );
--- src/utils/commonTypes.ts
+++ src/utils/commonTypes.ts
@@ -23,4 +23,50 @@ interface PageInfo {
   current: number;
 }
 
-export type { CategoryArray, MetaData, Conversation, ConversationArray, PageInfo };
+type Item = {
+  [index: string]: number | string; // 참고: https://soopdop.github.io/2020/12/01/index-signatures-in-typescript/
+  이름: string;
+  name: string;
+  카테고리A: string;
+  일회성: string;
+  가격: number;
+  '사용 효과 (w. tag)': string;
+  '사용 효과': string;
+  '사용 효과 쿨타임': number | string;
+  '지속 효과 (w. tag)': string;
+  '지속 효과': string;
+  '지속 효과 쿨타임': number | string;
+  '물리 공격력': number | string;
+  '물리 관통력': number | string;
+  '물리 방어력 무효화': number | string;
+  마법공격력: number | string;
+  '마법 관통력': number | string;
+  '마법 방어력 무효화': number | string;
+  '치명타 확률': number | string;
+  '공격 속도': number | string;
+  '생명력 흡수': number | string;
+  '물리 피해 흡혈': number | string;
+  '마법 피해 흡혈': number | string;
+  '모든 피해 흡혈': number | string;
+  '물리 방어력': number | string;
+  '마법 방어력': number | string;
+  체력: number | string;
+  마나: number | string;
+  '체력 재생': number | string;
+  '마나 재생': number | string;
+  '군중 제어 저항': number | string;
+  '재사용 대기 시간 감소': number | string;
+  '이동 속도': number | string;
+  '골드 가치': number;
+  하위템_List: string;
+  '이미지 path': string;
+};
+type ItemArray = Array<Item>;
+
+interface Attribute {
+  attribute_en: string;
+  attribute_kr: string;
+}
+type AttributeArray = Array<Attribute>;
+
+export type { CategoryArray, MetaData, Conversation, ConversationArray, PageInfo, Item, ItemArray, AttributeArray };
