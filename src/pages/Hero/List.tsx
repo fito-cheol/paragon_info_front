76f98cac82dd7fe1bf0c66ee1c53e6dd7553ba61
iuaena918@gmail.com <bjh9287@naver.com>
2022-12-30 16:35:16 +0900

    [feat] 영웅카드 UI

--- src/pages/Hero/List.tsx
+++ src/pages/Hero/List.tsx
@@ -1,5 +1,40 @@
 import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
+import images from 'assets/hero/imagePreloaderHero';
+import imageHeroTypes from 'assets/hero/imagePreloaderType';
+
+import './FullCard.scoped.scss';
 
 export default function List() {
-  return <h2> TO DO: Make Hero List </h2>;
+  return (
+    <>
+      <section>
+        <Card className='fullCard' sx={{ maxWidth: 208 }}>
+          <CardMedia
+            className='fullCardMedia'
+            component='img'
+            height='320'
+            image={images['Aurora']}
+            alt='영웅 오로라'
+          />
+          <CardContent className='fullCardContent'>
+            <CardMedia
+              className='iconImage'
+              image={imageHeroTypes['Wizard_white']}
+              component='img'
+              alt='영웅 타입'
+              sx={{ width: 45 }}
+            />
+            <Typography className='fullCardTypography' variant='h5' component='div'>
+              영웅이름
+            </Typography>
+          </CardContent>
+        </Card>
+      </section>
+    </>
+  );
 }
