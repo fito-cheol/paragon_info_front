60176b7bdc5d7c797eff8c3c806416b630755db9
김철 <dmsghs92@gmail.com>
2023-01-05 15:04:28 +0900

    Fix Error in CD pm2: command not found - ((pm2 describe))

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -4,9 +4,9 @@ REPOSITORY=/home/ubuntu/frontServer
 
 cd $REPOSITORY
 
-pm2 describe front_server > /dev/null
+sudo pm2 desc front_server > /dev/null
 RUNNING=$?
 
 if [ "${RUNNING}" -ne 0 ]; then
-  pm2 serve build/ 3000 --name front_server
+  sudo pm2 serve build/ 3000 --name front_server
 fi;
