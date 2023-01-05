b4874b7395525a001025374ae31d0ec7e7e574c9
김철 <dmsghs92@gmail.com>
2023-01-05 15:15:37 +0900

    Test CD

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -4,9 +4,9 @@ REPOSITORY=/home/ubuntu/frontServer
 
 cd $REPOSITORY
 
-sudo pm2 desc front_server > /dev/null
+pm2 desc front_server > /dev/null
 RUNNING=$?
 
 if [ "${RUNNING}" -ne 0 ]; then
-  sudo pm2 serve build/ 3000 --name front_server
+  pm2 serve build/ 3000 --name front_server
 fi;
