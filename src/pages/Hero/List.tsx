40e635d72fc3cbcab6acaf92189e863fd4541197
fito-juhee <bjh9287@naver.com>
2023-01-12 20:09:35 +0900

    영웅리스트 불러와서 뿌리기(스타일 이쁘게)

--- src/pages/Hero/List.tsx
+++ src/pages/Hero/List.tsx
@@ -1,39 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 
-import images from 'assets/hero/imagePreloaderHero';
-import imageHeroTypes from 'assets/hero/imagePreloaderType';
+import ImageHeros from 'assets/hero/imagePreloaderHero';
+import ImageHeroTypes from 'assets/hero/imagePreloaderType';
+
+import HeroJson from 'assets/hero/DB_Hero.json';
 
 import './FullCard.scoped.scss';
 
 export default function List() {
+  const [heroList, setHeroList] = useState<Hero[]>([]);
+  console.log('HeroJson', HeroJson);
+  console.log('ImageHeros', ImageHeros);
+  useEffect(() => {
+    setHeroList(HeroJson);
+  }, []);
+
   return (
     <>
-      <section>
-        <Card className='fullCard' sx={{ maxWidth: 208 }}>
-          <CardMedia
-            className='fullCardMedia'
-            component='img'
-            height='320'
-            image={images['Aurora']}
-            alt='영웅 오로라'
-          />
-          <CardContent className='fullCardContent'>
+      <section className='content'>
+        {heroList.map(hero => (
+          <Card key={hero.name} className='fullCard' sx={{ maxWidth: 208 }}>
             <CardMedia
-              className='iconImage'
-              image={imageHeroTypes['Wizard_white']}
+              className='fullCardMedia'
               component='img'
-              alt='영웅 타입'
-              sx={{ width: 45 }}
+              width='208'
+              height='320'
+              image={ImageHeros[hero.name]}
+              alt={hero.이름}
             />
-            <Typography className='fullCardTypography' variant='h5' component='div'>
-              영웅이름
-            </Typography>
-          </CardContent>
-        </Card>
+            <CardContent className='fullCardContent'>
+              <CardMedia
+                className='iconImage'
+                image={ImageHeroTypes[hero.category]}
+                component='img'
+                alt={hero.category}
+                sx={{ width: 45 }}
+              />
+              <Typography className='fullCardTypography' variant='h5' component='div'>
+                {hero.이름}
+              </Typography>
+            </CardContent>
+          </Card>
+        ))}
       </section>
     </>
   );
