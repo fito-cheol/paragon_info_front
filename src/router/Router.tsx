6f4c2bff396c331631625a67d0339b1aff9e1f90
김철 <dmsghs92@gmail.com>
2023-02-17 10:15:11 +0900

    Add Lazy

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,34 +1,38 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 
 import { Routes, Route, Navigate } from 'react-router-dom';
 
-import ItemList from '../pages/Item/List';
-import HeroList from '../pages/Hero/List';
-import HeroDetails from '../pages/Hero/Details';
-import TestPage from '../pages/Test';
-import TierHeroes from 'pages/Tier/Heroes';
-import RedirectPage from '../pages/Redirect/Google';
-import Write from '../pages/Walkthrough/Write';
-import List from '../pages/Walkthrough/List';
+const ItemList = lazy(() => import('pages/Item/List'));
+const HeroList = lazy(() => import('pages/Hero/List'));
+const HeroDetails = lazy(() => import('pages/Hero/Details'));
+const TestPage = lazy(() => import('pages/Test'));
+const TierHeroes = lazy(() => import('pages/Tier/Heroes'));
+const RedirectPage = lazy(() => import('pages/Redirect/Google'));
+const Write = lazy(() => import('pages/Walkthrough/Write'));
+const List = lazy(() => import('pages/Walkthrough/List'));
 
-import VideoList from 'pages/Video/List';
-import Main from 'pages/Home/Main';
+const VideoList = lazy(() => import('pages/Video/List'));
+const Main = lazy(() => import('pages/Home/Main'));
+
+const renderLoader = () => <p>Loading</p>;
 
 export default function Router() {
   return (
-    <Routes>
-      <Route path='item' element={<ItemList />} />
-      <Route path='hero' element={<HeroList />} />
-      <Route path='hero/:name' element={<HeroDetails />} />
-      <Route path='write' element={<Write />} />
-      <Route path='modify/:no' element={<Write />} />
-      <Route path='list' element={<List />} />
-      <Route path='test' element={<TestPage />} />
-      <Route path='redirect' element={<RedirectPage />} />
-      <Route path='tier' element={<TierHeroes />} />
-      <Route path='youtube' element={<VideoList />} />
-      <Route path='' element={<Main />} />
-      <Route path='*' element={<Navigate to='/' replace />} />
-    </Routes>
+    <Suspense fallback={renderLoader()}>
+      <Routes>
+        <Route path='item' element={<ItemList />} />
+        <Route path='hero' element={<HeroList />} />
+        <Route path='hero/:name' element={<HeroDetails />} />
+        <Route path='write' element={<Write />} />
+        <Route path='modify/:no' element={<Write />} />
+        <Route path='list' element={<List />} />
+        <Route path='test' element={<TestPage />} />
+        <Route path='redirect' element={<RedirectPage />} />
+        <Route path='tier' element={<TierHeroes />} />
+        <Route path='youtube' element={<VideoList />} />
+        <Route path='' element={<Main />} />
+        <Route path='*' element={<Navigate to='/' replace />} />
+      </Routes>
+    </Suspense>
   );
 }
